0efe6fa3fd41e32e5ee0ef1138c4ca73ee9ee6a4
Fahd <fahd.rafi@gmail.com>
2013-11-01 12:04:36 +0000

    Minor changes.

--- app/controllers/Chataan.scala
+++ app/controllers/Chataan.scala
@@ -13,32 +13,30 @@ import scala.xml.{Node, NodeSeq, Elem}
  */
 
 object XMLize {
-  def toPList[T](x: T) = {
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" +
-      <pList version="1.0">{XMLize.toXML(x)}</pList>
-  }
-  def toXML[T](x: T): Elem = x match {
+  private def toPListXML[T](x: T): Elem = x match {
     case e: String => <string>{e}</string>
     case e: Double => <real>{e}</real>
     case e: Integer => <integer>{e}</integer>
-    case true => <true/> case false => <false/>
-    case m: Map[String,AnyRef] => <dict/>.copy(child = m.map(e => List(<key>{e._1}</key>,XMLize.toXML(e._2))).flatten.toSeq)
-    case l: List[AnyRef] => <array/>.copy(child = l.map(e => XMLize.toXML(e)).toSeq)
+    case true => <true/>
+    case false => <false/>
+    case m: Map[String,AnyRef] => <dict/>.copy(child = m.map(e => List(<key>{e._1}</key>,XMLize.toPListXML(e._2))).flatten.toSeq)
+    case l: List[AnyRef] => <array/>.copy(child = l.map(e => XMLize.toPListXML(e)).toSeq)
     case e => <unknown>{e.toString}</unknown>
+  }
+  def toPList[T](x: T) = {
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" +
+      <pList version="1.0">{XMLize.toPListXML(x)}</pList>
   }
 }
 
 object Chataan extends Controller {
-  val trueFalseList = List(true, false, false)
-  val sampleMap = Map(
+  val sampleResponse = Map(
     "key1" -> "string value here",
-    "key2" -> List (
-      0.1, 0.5, 0.4, 0.6, 0.1, 0.7, 0.2, 0.9
-    ),
+    "key2" -> List (0.1, 0.5, 0.4, 0.6, 0.1, 0.7, 0.2, 0.9),
     "a number" -> 5,
-    "trueFalseList" -> trueFalseList
+    "trueFalseList" -> List(true, false, false)
   )
   def topEntities = Action {
-    Ok(XMLize.toPList(sampleMap)).as("application/xml")
+    Ok(XMLize.toPList(sampleResponse)).as("application/xml")
   }
 }
